c6cb6e81f7fcb48c42da094b697bf9aaa32936f8
Sabas Gonzalez <46977278+SabasGT@users.noreply.github.com>
2023-02-19 16:08:02 -0400

    Delete Tarea (Falta Timeline) directory

--- Tarea (Falta Timeline)/addTweetToListFunc.h
+++
@@ -1,6 +0,0 @@
-#ifndef ADDTWEETTOLIST
-#define ADDTWEETTOLIST
-
-void addTweetToList(struct tweet * firstTweetOfUser, char newTweet[280], char userThatSentTheTweet[20]);
-
-#endif
--- Tarea (Falta Timeline)/checkIfUserIsFollowedFunc.h
+++
@@ -1,7 +0,0 @@
-#include "estructuras.h"
-#ifndef CHECKIFUSERISFOLLOWED
-#define CHECKIFUSERISFOLLOWED
-
-int checkIfUserIsFollowed (struct userFollowed* userFollowedToCheck, char userToCheckIfItsFollowed[20]);
-
-#endif
